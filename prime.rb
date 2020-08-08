e4d77eaa3d3a104cb0bf65dd07b8126e1a80a1fd
Sara A <calisaraalhindi@gmail.com>
2020-08-08 18:31:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,17 @@
-def prime?(num)
-  return false if num < 2
-  (2... num/2).none?{|i| n % i == 0}
-end
-end
+ def is_prime n
+  for d in 2..(n - 1)
+   if (n % d) == 0
+    return false
+   end
+  end
 
-  # if num > 1
-    #range = (i.. num-1).to_a
-    #true
-  #else
-    #false
-  #end Add  code here!
+  true
+ end
+
+ def is_prime2 n
+  foundDivider = false
+   for d in 2..(n - 1)
+    foundDivider = ((n % d) == 0) or foundDivider
+   end
+  not foundDivider
+ end
